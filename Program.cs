5dba1b2f97a6cbf7b393154ae12e91cbebaac1a9
uberdi <disinyakin@mail.ru>
2023-02-05 18:34:18 +0400

    add code

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
-namespace ConsoleApp8
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-  class Program
-  {
-    static void Main(string[] args)
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-      // Инициализация массива строк
-      string[] daysOfWeek = { "Sunday", "Monday", "Tuersday",
-      "Wednesday", "Thirsday", "Friday", "Saturday" };
-
-      // Вывод массива строк AS в цикле
-      for (int i = 0; i < daysOfWeek.Length; i++)
-        Console.WriteLine("AS[{0}] = {1}", i, daysOfWeek[i]);
-      Console.ReadKey();
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
-  }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
