d90c6d8a9d97856e60c55d5854eb6524919e2333
uberdi <disinyakin@mail.ru>
2023-02-05 18:26:44 +0400

    add readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Инициализация массива строк
+      string[] daysOfWeek = { "Sunday", "Monday", "Tuersday",
+      "Wednesday", "Thirsday", "Friday", "Saturday" };
+
+      // Вывод массива строк AS в цикле
+      for (int i = 0; i < daysOfWeek.Length; i++)
+        Console.WriteLine("AS[{0}] = {1}", i, daysOfWeek[i]);
+      Console.ReadKey();
+    }
+  }
+}
